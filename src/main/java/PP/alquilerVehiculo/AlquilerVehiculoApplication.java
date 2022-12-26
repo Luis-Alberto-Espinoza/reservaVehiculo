a34002ee5dca17b03739e3d2a6f096ab21fc1f82
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2022-12-25 21:44:43 -0300

    creacion injeccionDependencia configuracion proyecto

---
+++ src/main/java/PP/alquilerVehiculo/AlquilerVehiculoApplication.java
@@ -0,0 +1,13 @@
+package PP.alquilerVehiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerVehiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilerVehiculoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PP/alquilerVehiculo/AlquilerVehiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package PP.alquilerVehiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerVehiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
