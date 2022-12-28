43c8eff5261ab1fb2e616406dc559497cf9f0c3a
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2022-12-28 12:55:38 -0300

    muestra los empleados

--- src/main/java/PP/alquilerVehiculo/controladores/EmpleadoControlador.java
+++ src/main/java/PP/alquilerVehiculo/controladores/EmpleadoControlador.java
@@ -1,16 +1,41 @@
 package PP.alquilerVehiculo.controladores;
 
+import PP.alquilerVehiculo.entidad.Empleado;
+import PP.alquilerVehiculo.servicio.EmpleadoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
+import java.util.List;
+
 @Controller
 @RequestMapping("/empleado")
 public class EmpleadoControlador {
-    @GetMapping("/mostrar_e")
-    public String mostrar_e(){
-        return  "indexe";
+    @Autowired
+    private EmpleadoServicio empleadoServicio;
+
+    @GetMapping("/tabla")
+    public String mostrar_e(ModelMap modelo) throws Exception {
+        List<Empleado> listaEmpleados = empleadoServicio.findAll();
+        modelo.addAttribute("empleados", listaEmpleados);
+
+        return "hitorial_reserva_cliente";
+    }
+
+    @PostMapping("/mostrar")
+    public String mostrar(@RequestParam String email) {
+        try {
+            empleadoServicio.guardarMail(email);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return "indexe";
     }
 
 }
--- src/main/java/PP/alquilerVehiculo/servicio/EmpleadoServicio.java
+++ src/main/java/PP/alquilerVehiculo/servicio/EmpleadoServicio.java
@@ -1,15 +1,26 @@
 package PP.alquilerVehiculo.servicio;
 
 import PP.alquilerVehiculo.entidad.Empleado;
+import PP.alquilerVehiculo.repositorios.EmpleadoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
 public class EmpleadoServicio implements BaseService<Empleado> {
-
+    @Autowired
+    EmpleadoRepositorio empleadoRepositorio;
+@Transactional
+    public void guardarMail(String mail)throws Exception {
+        Empleado empleado = new Empleado();
+        empleado.setMail(mail);
+        empleadoRepositorio.save(empleado);
+    }
     @Override
     public List<Empleado> findAll() throws Exception {
-        return null;
+    List<Empleado> listaEmpleados = empleadoRepositorio.findAll();
+        return listaEmpleados;
     }
 
     @Override
